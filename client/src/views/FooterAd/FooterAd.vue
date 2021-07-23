beae6cc3bae6be5b948629fc042b4113b5f2e90b
Junho Kim <kimjunho7750@gmail.com>
2021-07-23 16:08:53 +0900

    barcode page
    Former-commit-id: 00a98d4ba2e6e472f11fc091259eadb6a9deb2e2

---
+++ client/src/views/FooterAd/FooterAd.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="row">
+    <div class="column">
+      <img src="@/assets/item1.png" alt="item1" style="width:100%">
+    </div>
+    <div class="column">
+      <img src="@/assets/item2.png" alt="item2" style="width:100%">
+    </div>
+    <div class="column">
+      <img src="@/assets/item3.png" alt="item3" style="width:100%">
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+/* Three image containers (use 25% for four, and 50% for two, etc) / */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+/* / Clear floats after image containers */
+.row {
+  display: flex;
+  content: "";
+  clear: both;
+}
+
+</style>
