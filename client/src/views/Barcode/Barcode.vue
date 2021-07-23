9bc891f65ff3916e6eaf4eaf91e83a587a48661d
김다윗 <kimdawit7@hanmail.net>
2021-07-23 11:16:57 +0900

    Init

---
+++ client/src/views/Barcode/Barcode.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>Barcode</h4>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Main/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img alt="zara logo" src="https://cdn.iconscout.com/icon/free/png-512/zara-3215529-2673947.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
