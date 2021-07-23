f827b92f181517fb7403626c7fe46f82d58ba3ab
Junho Kim <kimjunho7750@gmail.com>
2021-07-23 16:30:35 +0900

    Nav page
    Former-commit-id: cea1c98bc3a6c8b78d26459f303ccfbecafcf0e4

--- client/src/views/Nav/Nav.vue
+++ client/src/views/Nav/Nav.vue
@@ -1,9 +1,15 @@
 <template>
-  <img src="@/assets/logo.png" alt="logo" class="logo">
+  <img src="@/assets/logo.png" alt="logo" class="logo" @click="gotoMain()">
 </template>
 
 <script>
 export default {
+  name: 'Nav',
+  methods: {
+    gotoMain() {
+      this.$router.push('/');
+    }
+  },
 
 }
 </script>
