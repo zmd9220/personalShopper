00a98d4ba2e6e472f11fc091259eadb6a9deb2e2
Junho Kim <kimjunho7750@gmail.com>
2021-07-23 16:08:53 +0900

    barcode page

---
+++ client/src/views/Nav/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <img src="@/assets/logo.png" alt="logo" class="logo">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.logo {
+  display: flex;
+  width: 200px;
+}
+</style>
