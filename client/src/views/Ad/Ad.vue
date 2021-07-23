4c5b3e539282ec0da7edaa526ec240b83dfd8235
김다윗 <kimdawit7@hanmail.net>
2021-07-23 17:40:46 +0900

    feature/ad

--- client/src/views/Ad/Ad.vue
+++ client/src/views/Ad/Ad.vue
@@ -1,13 +1,74 @@
 <template>
-  <h4>Ad test</h4>
+  <div >
+    <header>
+      <SearchBar @search="onSearch"/>
+    </header>
+
+    <section>
+      <VideoDetail :selectedVideo="selectedVideo"/>
+      <VideoList :videoList="videoList" @select-video="onSelectedVideo"/>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import SearchBar from '@/views/Ad/SearchBar.vue'
+import VideoList from '@/views/Ad/VideoList.vue'
+import VideoDetail from '@/views/Ad/VideoDetail.vue'
 
+export default {
+  name: 'Ad',
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail,
+  },
+  data: function () {
+    return {
+      videoList: [],
+      selectedVideo: null,
+    }
+  },
+  methods: {
+    onSearch: function (query) {
+      this.query = query
+      const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
+      const API_URL = 'https://www.googleapis.com/youtube/v3/search'
+
+      const params = {
+        key: API_KEY,
+        part: 'snippet',
+        type: 'video',
+        q: query,
+      }
+
+      axios({
+        method: 'GET',
+        url: API_URL,
+        params,
+      }).then(response => {
+        this.videoList = response.data.items
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    onSelectedVideo: function (video) {
+      this.selectedVideo = video
+    }
+  }
 }
 </script>
 
 <style>
+header {
+  width: 80%;
+  margin: 0 auto;
+  padding: 1rem 0;
+}
 
+section {
+  display: flex;
+  
+}
 </style>
