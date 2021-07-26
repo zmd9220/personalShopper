3bb68d44c3823211c9f469869f0c7ac81383cf5d
김다윗 <kimdawit7@hanmail.net>
2021-07-26 11:23:32 +0900

    S05P12B201-15 Feat: 기본 template 추가 작업 상품상세 화면 작성

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,7 +4,13 @@ import Main from '@/views/Main/Main'
 import Ad from '@/views/Ad/Ad'
 import AdClient from '@/views/Ad/AdClient'
 import Barcode from '@/views/Barcode/Barcode' 
-import PersonalShopper from '@/views/PersonalShopper/PersonalShopper'
+import PersonalShopper from '@/views/PersonalShopper/PersonalShopper' 
+import ProductDetail from '@/views/ProductDetail/ProductDetail'
+import ProductSizeChart from '@/views/ProductDetail/ProductSizeChart' 
+import ProductDetailLocation from '@/views/ProductDetail/ProductDetailLocation'  
+import ProductSizeRecommand from '@/views/ProductDetail/ProductSizeRecommand'
+
+
 
 Vue.use(VueRouter)
 
@@ -33,6 +39,26 @@ const routes = [
     path: '/PersonalShopper',
     name: 'PersonalShopper',
     component: PersonalShopper
+  },
+  {
+    path: '/ProductDetail',
+    name: 'ProductDetail',
+    component: ProductDetail
+  },
+  {
+    path: '/ProductSizeChart',
+    name: 'ProductSizeChart',
+    component: ProductSizeChart
+  },
+  {
+    path: '/ProductDetailLocation',
+    name: 'ProductDetailLocation',
+    component: ProductDetailLocation  
+  },
+  {
+    path: '/ProductSizeRecommand',
+    name: 'ProductSizeRecommand',
+    component: ProductSizeRecommand  
   },
 
 ]
