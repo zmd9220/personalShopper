ed6ce3854d8cd75f47aaa0bb3ff1b3d0bb2d3dc6
김다윗 <kimdawit7@hanmail.net>
2021-07-26 11:23:32 +0900

    S05P12B201-15 Feat: 기본 template 추가 작업 상품상세 화면 작성
    Former-commit-id: 3bb68d44c3823211c9f469869f0c7ac81383cf5d

---
+++ client/src/views/ProductDetail/ProductSizeRecommand.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <Nav/>
+    <div class=""> <h1>상품이름</h1></div>
+    <div class="ProductBox container">
+      <img class="ProductDetailImg" src="@/assets/item1.png" alt="item1" style="width:45%">
+      <div class="TextBox">
+        
+
+      </div>
+    </div>
+
+    
+    <h4>PersonalShopper의 추천</h4>
+    <FooterAd/>
+  </div>
+</template>
+
+<script>
+import FooterAd from '@/views/FooterAd/FooterAd'
+import Nav from '@/views/Nav/Nav'
+
+export default {
+  name: 'Barcode',
+  components: {
+    FooterAd,
+    Nav,
+  },
+}
+</script>
+
+<style>
+.ProductBox {
+  display: flex;
+  margin-bottom: 5rem;
+}
+
+.ProductDetailImg {
+  margin-right: 5rem;
+}
+
+.TextBox {
+  text-align: center;
+}
+
+</style>
