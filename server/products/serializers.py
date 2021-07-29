59b87f70abccda8fb69b0761bdf18e6163da27f8
Junho Kim <kimjunho7750@gmail.com>
2021-07-29 17:43:20 +0900

    S05P12B201-86 Feat: 상품 전체조회, 상품 상세조회 백엔드 기능 추가

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -1,23 +1,23 @@
-# from django.db import models
-# from .models import Product, Customer, Stock
-# from rest_framework import serializers
+from django.db import models
+from .models import Product, Customer, Stock
+from rest_framework import serializers
 
-# class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
 
-#     class Meta:
-#         model = Product
-#         fields = '__all__'
-
-
-# class CustomerSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Customer
-#         fields = '__all__'
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 
-# class StockSerializer(serializers.ModelSerializer):
+class CustomerSerializer(serializers.ModelSerializer):
 
-#     class Meta:
-#         model = Stock
-#         fields = '__all__'
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class StockSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Stock
+        fields = '__all__'
