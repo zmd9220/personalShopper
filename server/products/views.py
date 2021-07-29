15c88c5f78114d56fa99a83b0d6b9beb67409380
김다윗 <kimdawit7@hanmail.net>
2021-07-29 15:31:07 +0900

    S05P12B201-87 Docs: 더미데이터 load 시키기

--- server/products/views.py
+++ server/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# from .models import Product, Customer, Stock
+# from .serializers import ProductSerializer, CustomerSerializer, StockSerializer
 
 # Create your views here.
+
