dadc072e2e6c92b8735647e7671cf8ad5d3cadcd
Junho Kim <kimjunho7750@gmail.com>
2021-07-29 17:43:20 +0900

    S05P12B201-86 Feat: 상품 전체조회, 상품 상세조회 백엔드 기능 추가
    Former-commit-id: 59b87f70abccda8fb69b0761bdf18e6163da27f8

--- server/products/views.py
+++ server/products/views.py
@@ -1,9 +1,26 @@
-# from django.shortcuts import render
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# from .models import Product, Customer, Stock
-# from .serializers import ProductSerializer, CustomerSerializer, StockSerializer
+# from rest_framework.decorators import authentication_classes, permission_classes
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-# Create your views here.
+from .serializers import ProductSerializer, CustomerSerializer, StockSerializer
+from .models import Product, Customer, Stock
 
+@api_view(['GET'])
+def product_index(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializers = ProductSerializer(products, many=True)
+        return Response(serializers.data)
+
+
+@api_view(['GET'])
+def product_detail(request, product_id):
+    if request.method == 'GET':
+        product = get_object_or_404(Product, product_id = product_id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
