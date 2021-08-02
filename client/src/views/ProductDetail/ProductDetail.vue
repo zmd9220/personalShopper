99365114ba65b1eabc9312069bb0772586a60917
Junho Kim <kimjunho7750@gmail.com>
2021-08-02 16:21:34 +0900

    S05P13B201-29 Feat: axios 추가, cors 설정
    Former-commit-id: b65c71dd5b26a7474744909bd1a1bd421a9492a8

--- client/src/views/ProductDetail/ProductDetail.vue
+++ client/src/views/ProductDetail/ProductDetail.vue
@@ -2,15 +2,16 @@
   <div>
     <Nav/>
     <div class="product-box container">
-      <img class="product-detail-img" src="@/assets/item1.png" alt="item1" style="width:45%">
+      <img class="product-detail-img" src="@/assets/dummydata/productDetail.product_image" alt="item1" style="width:45%">
       <div class="text-box">
         <!-- 상품이름 연동필요 -->
-        <h2 class="text-box-title">스트럭처 셔츠</h2> 
+        <h2 class="text-box-title">{{ productDetail.product_name }}</h2> 
         <h2>재고정보</h2>
         <div class="size-box"></div>
         <h2 @click="goToSizeRecommend()">사이즈 추천받기</h2>
         <h2 @click="goToSizeChart()">사이즈표</h2>
         <h2 @click="goToLocation()">상품위치 정보</h2>
+        <button @click="getItem()">이걸눌러라</button>
       </div>
     </div>
 
@@ -23,12 +24,18 @@
 <script>
 import FooterAd from '@/views/FooterAd/FooterAd'
 import Nav from '@/views/Nav/Nav'
+import axios from 'axios'
 
 export default {
   name: 'ProductDetail',
   components: {
     FooterAd,
     Nav,
+  },
+  data: function() {
+    return {
+      productDetail: '',
+    }
   },
   methods :{
     goToSizeRecommend(){
@@ -40,9 +47,24 @@ export default {
     goToLocation(){
       this.$router.push('/ProductDetailLocation'); 
     },
+    getItem: function() {
+      axios.get('http://127.0.0.1:8000/product/101/')
+        .then((res) => {
+          console.log(res);
+          this.productDetail = res.data
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
   },
+  created: function () {
+    this.getItem()    
+  }
 }
+
 </script>
+
 
 <style scoped>
 .product-box {
