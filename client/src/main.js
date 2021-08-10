bd6dd039f8578183b969189915581bfe62c52baa
육승준 <6seungjun@naver.com>
2021-08-10 10:49:33 +0900

    S05P13B201-200 Feat:vuex 추가
    Former-commit-id: ae08c35f9073b048b5315222158b2fcd32f6eae5

--- client/src/main.js
+++ client/src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -14,5 +15,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
