f6eecb280ffe66a82861f11dabefaa46bbcbfc6e
Junho Kim <kimjunho7750@gmail.com>
2021-08-13 10:32:19 +0900

    S05P13B201-209 Feat: 장바구니 구현

---
+++ client/src/views/PersonalShopper/PersonalShopperDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>sadlkl</h1>  
+    <FooterAd/>
+  </div>
+</template>
+
+<script>
+import FooterAd from '@/views/FooterAd/FooterAd'
+
+export default {
+  name: 'PersonalShopperDetail',
+  components : {
+    FooterAd,
+  }
+}
+</script>
+
+<style>
+
+</style>
