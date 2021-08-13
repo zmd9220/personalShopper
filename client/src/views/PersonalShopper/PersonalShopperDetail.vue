688298d5ab2b3196eb2a0cc31e17b0aca20e56fe
육승준 <6seungjun@naver.com>
2021-08-13 17:39:12 +0900

    S05P13B201-223 Feat: 퍼스널 쇼퍼 상세페페이지footer-router

--- client/src/views/PersonalShopper/PersonalShopperDetail.vue
+++ client/src/views/PersonalShopper/PersonalShopperDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Nav/>
-    <p class="title">"Look" for you</p>  
+    <p class="title">"Look" 4 U</p>  
     <div class="image-area">
       <b-icon class="img-switch-prev" icon="arrow-left-circle-fill" aria-hidden="true"></b-icon>
       <img :src="productDetail.style_image" alt="look1" class="look-detail">
@@ -19,10 +19,10 @@
       </div>
       <br>
       <div class="class=look-items">
-        <img class="look-item" :src="productDetail.product_image" alt="item1">
-        <img class="look-item" :src="this.$store.state.productRecommend_1" alt="item2">
-        <img class="look-item" :src="this.$store.state.productRecommend_2" alt="item3">
-        <img class="look-item" :src="this.$store.state.productRecommend_3" alt="item4">
+        <img class="look-item" :src="productDetail.product_image" alt="item1" @click="gotoDetail1()" >
+        <img class="look-item" :src="this.$store.state.productRecommend_1" alt="item2" @click="gotoDetail2()" >
+        <img class="look-item" :src="this.$store.state.productRecommend_2" alt="item3" @click="gotoDetail3()" >
+        <img class="look-item" :src="this.$store.state.productRecommend_3" alt="item4" @click="gotoDetail4()" >
       </div>
     </div>
     <!-- <FooterAd/> -->
@@ -48,6 +48,42 @@ export default {
   methods: {
     addToCart(productDetail) {
       this.$store.dispatch('cart/addItem', productDetail);
+    },
+    gotoDetail1: function() {                
+      if (this.$route.path !== '/ProductDetail' ) {
+        this.$store.commit('selectProductID', this.$store.state.productDetail.product_id);
+        this.$router.push({name:'ProductDetail'});
+      } else {
+        this.$store.commit('selectProductID', this.$store.state.productDetail.product_id);
+        this.$emit('selectedProductId', this.$store.state.productDetail.product_id);
+      }
+    },
+    gotoDetail2: function() {                 
+      if (this.$route.path !== '/ProductDetail' ) {
+        this.$store.commit('selectProductID', this.$store.state.productId_1);
+        this.$router.push({name:'ProductDetail'});
+      } else {
+        this.$store.commit('selectProductID', this.$store.state.productId_1);
+        this.$emit('selectedProductId', this.$store.state.productId_1);
+      }
+    },
+    gotoDetail3: function() {                 
+      if (this.$route.path !== '/ProductDetail' ) {
+        this.$store.commit('selectProductID', this.$store.state.productId_2);
+        this.$router.push({name:'ProductDetail'});
+      } else {
+        this.$store.commit('selectProductID', this.$store.state.productId_2);
+        this.$emit('selectedProductId', this.$store.state.productId_2);
+      }
+    },
+    gotoDetail4: function() {                 
+      if (this.$route.path !== '/ProductDetail' ) {
+        this.$store.commit('selectProductID', this.$store.state.productId_3);
+        this.$router.push({name:'ProductDetail'});
+      } else {
+        this.$store.commit('selectProductID', this.$store.state.productId_3);
+        this.$emit('selectedProductId', this.$store.state.productId_3);
+      }
     },
   }
 }
