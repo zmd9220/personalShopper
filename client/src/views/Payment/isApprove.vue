f9dca9f04195848f8231d62bc663bd797b03fb81
안병진 <zmd9220@gmail.com>
2021-08-15 21:35:22 +0900

    S05P13B201-253 Feat : 결제 승인 시 recommend 테이블에 해당 나이 판매량 +1

--- client/src/views/Payment/isApprove.vue
+++ client/src/views/Payment/isApprove.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <p>{{ $route.query }}</p>
+    <!-- <p>{{ $route.query }}</p>
     <p>{{ paymentData.pg_token }}</p>
     <p>{{ paymentData.tid }} </p>
-    <p>{{ paymentData.orderNumber }} </p>
+    <p>{{ paymentData.orderNumber }} </p> -->
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import {mapState} from 'vuex'
 
 export default {
   name: 'isApprove',
@@ -24,14 +25,21 @@ export default {
   methods: {
     approve () {
       let baseUrl = "http://127.0.0.1:8000/"
-      console.log(this.paymentData)
+      // console.log(this.paymentData)
+      let requestData = this.paymentData
+      console.log(this.userData)
+      console.log(this.orderItems)
+      requestData.userData = this.userData
+      requestData.orderItems = this.orderItems
+      console.log(requestData)
       axios({
         method: 'POST',
         url: baseUrl + "kakaoPayApprove/",
-        data: this.paymentData,
+        data: requestData,
       }).then((res) =>{
           let responseData = res.data
           if (responseData.status_code === 200) {
+            localStorage.setItem('orderNumber', Number(localStorage.getItem('orderNumber')) + 1)  
             this.$router.push({name: 'OrderComplete', params: {responseData: responseData}})
           } else {
             alert('Error code : ' + responseData.code + '\n' + responseData.extras.method_result_message)
@@ -51,15 +59,13 @@ export default {
         let nowOrderNumber = localStorage.getItem('orderNumber')
         this.paymentData.tid = nowTid
         this.paymentData.orderNumber = nowOrderNumber
-        console.log(this.paymentData.tid)
-        console.log(this.paymentData.orderNumber)
       })
     },
   },
   mounted: function () {
     if (this.$route.query.pg_token) {
       // 정상적으로 토큰이 발급된 경우
-      console.log(this.$route.query)
+      // console.log(this.$route.query)
       this.paymentData.pg_token = this.$route.query.pg_token
       // this.makeData().then(() => {
       //   console.log('makeData com')
@@ -73,6 +79,14 @@ export default {
       // this.$router.push({name:'Payment', params: {product: localStorage.getItem('orderedProduct')}})
       this.$router.push({name: 'Cart'})
     }
+  },
+  computed: {
+    ...mapState('cart',{
+      orderItems: 'items',
+    }),
+    ...mapState({
+      userData: 'user',
+    }),
   }
 }
 </script>
