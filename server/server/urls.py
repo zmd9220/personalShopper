eae2d4081c89b9a19d58059b985e48d640285892
김다윗 <kimdawit7@hanmail.net>
2021-08-17 10:27:53 +0900

    S05P13B201-331 Feat: 백엔드 주석달기 및 readme 작성
    Former-commit-id: 2432cdb1fce6f2ca615eee2e72f25566ae418671

--- server/server/urls.py
+++ server/server/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from .routers import router
 from django.views.generic import TemplateView
-# from products.admin import kiosk_admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('kiosk_admin/', kiosk_admin.urls),
     path('', include('products.urls')), # 상품관련 url
     path('api/', include(router.urls)),
     path('article', TemplateView.as_view(template_name='index.html')),
